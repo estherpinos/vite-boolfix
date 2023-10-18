7362f15f48df1e4655d9beb863565e8ffabe9874
estherpinos <estherpinos1@gmail.com>
2023-10-18 12:10:22 +0200

    Add [menu]

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,12 +21,22 @@ export default{
     <div class="contents-bar d-flex justify-content-between align-items-center ">
       <div class="logo d-flex align-items-center ">
         <img src="logo-boolflix.png" alt="">
+        <div class="menu-header d-flex align-items-center ">
+          <ul class=" d-inline-block d-flex ">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Serie TV</a></li>
+            <li><a href="#">Film</a></li>
+            <li><a href="#">Originali</a></li>
+            <li><a href="#">Aggiunti di recente</a></li>
+            <li><a href="#">La mia lista</a></li>
+          </ul>
+        </div>
       </div>
     
       <div class="input-group mb-3 search d-flex align-items-center">
-        <input type="text" class="form-control" placeholder="Recipient's username" v-model="store.datoInput" >
+        <input type="text" class="form-control src-bar" placeholder="Recipient's username" v-model="store.datoInput" >
         <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button" @click="$emit ('go')">Button</button>
+          <button class="btn btn-outline-secondary btn-search" type="button" @click="$emit ('go')">Button</button>
         </div>
 
       </div>
@@ -42,9 +52,10 @@ export default{
 .bar{
   width: 100%;
   height: 60px;
-  padding: 20px;
     .contents-bar{
-      height: 100%;
+      height: 60px;
+      padding: 20px;
+    
     }
       .logo{
         & img{
@@ -53,8 +64,43 @@ export default{
       }
 
       .search{
-        width:200px;
+        width:220px;
+        height: 30px;
+        margin: 0;
+
+        .src-bar{
+          width: 150px;
+          height: 30px;
+          margin: 0;
+        }
+
+        .btn-search{
+          width: 70px;
+          height: 30px;
+        }
       }
+
+      .menu-header{
+        height: 100%;
+        align-items: center;
+        display: flex;
+        ul{
+        text-decoration: none;
+        margin: 0;
+        li{
+          text-decoration: none;
+          margin-right: 10px;
+  
+          a{
+            color: white;
+            text-decoration: none;
+            font-size: 12px;
+          }
+        }
+      }
+      }
+
+      
 }
 
 </style>
